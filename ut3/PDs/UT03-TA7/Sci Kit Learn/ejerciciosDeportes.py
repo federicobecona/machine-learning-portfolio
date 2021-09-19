3ee346e93c3aa4c0dd6a83ef129d5c262233bc7e
Federico Becona <federicobecona@gmail.com>
2021-09-19 16:43:41 -0300

    PD3 TA7

--- ut3/PDs/UT03-TA7/Sci Kit Learn/ejerciciosDeportes.py
+++ ut3/PDs/UT03-TA7/Sci Kit Learn/ejerciciosDeportes.py
@@ -9,9 +9,9 @@ from sklearn import preprocessing
 
 #Generating the model
 
-input_file = "sports_Training.csv"
+input_file = "../sports_Training.csv"
 data_original = pd.read_csv(input_file, header=0)
-print(data_original.values)
+#print(data_original.values)
 
 data = data_original[(data_original['CapacidadDecision'] >= 3) &(data_original['CapacidadDecision'] <= 100)]
 
@@ -27,12 +27,19 @@ lda = lda.fit(X, Y)
 
 #Scoring
 
-input_file2 = "sports_Scoring.csv"
-data_scoring = pd.read_csv(input_file, header=0)
+input_file2 = "../sports_Scoring.csv"
+data_scoring = pd.read_csv(input_file2, header=0)
 
-Xsco = data_scoring[['Edad','Fuerza','Velocidad','Lesiones','Vision','Resistencia','Agilidad','CapacidadDecision']].values
+data = data_scoring[(data_scoring['CapacidadDecision'] >= 3) &(data_scoring['CapacidadDecision'] <= 100)]
+
+Xsco = data[['Edad','Fuerza','Velocidad','Lesiones','Vision','Resistencia','Agilidad','CapacidadDecision']].values
 
 y_pred = lda.predict(Xsco)
- 
+
+df = pd.DataFrame({'Prediccion': y_pred})
+writer = pd.ExcelWriter('Prediccion.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
+writer.save()
+
 print("Predicted")
 print(y_pred)
