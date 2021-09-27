466c4225cdf674110c38bf7c9e08f0b03b9c8635
Federico Becona <federicobecona@gmail.com>
2021-09-27 20:00:02 -0300

    Mejoras PD2 UT2

--- ut2/PDs/UT2-PD3/script.py
+++ ut2/PDs/UT2-PD3/script.py
@@ -2,6 +2,9 @@ from csv import reader
 from math import sqrt
 from random import randrange
 import pandas as pd
+import copy
+
+columns = ['Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols','Proanthocyanins','Color intensity','Hue','OD280/OD315 of diluted wines','Proline']
 
 def load_csv(filename):
     dataset = list()
@@ -44,8 +47,8 @@ def column_stdevs(dataset, means):
 def normalize_dataset(dataset, minmax):
     for row in dataset:
         for i in range(len(row)):
-            row[i] = (row[i] - minmax[i][0])
-            (minmax[i][1] - minmax[i][0])
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
 
 def standardize_dataset(dataset, means, stdevs):
     for row in dataset:
@@ -61,21 +64,38 @@ def train_test_split(dataset, split=0.60):
         train.append(dataset_copy.pop(index))
     return train, dataset_copy
 
-
 input_file = "wine.csv"
 dataset = load_csv(input_file)
+print("\n- Primeras 10 filas:")
 for i in range(10):
     print(dataset[i])
-for i in range(14):
-    str_column_to_float(dataset,i)
-minmax = dataset_minmax(dataset)
-means = column_means(dataset)
-stdevs = column_stdevs(dataset, means)
-print(minmax)
-print(means)
-print(stdevs)
-normalized_dataset = dataset.copy()
+strToFloatDataset = copy.deepcopy(dataset)
+for i in range(len(columns)+1):
+    str_column_to_float(strToFloatDataset,i)
+print("\n- Primeras 10 filas dataset con floats:")
+for i in range(10):
+    print(strToFloatDataset[i])
+minmax = dataset_minmax(strToFloatDataset)
+means = column_means(strToFloatDataset)
+stdevs = column_stdevs(strToFloatDataset, means)
+print("\n- Estadísticas: ")
+for i in range(len(columns)):
+    print("*"+columns[i] + ": min " + str(minmax[i][0]) + ",  max " + str(minmax[i][1]) + ", media " + str(means[i]) + ", desviación estándar " + str(stdevs[i]))
+normalized_dataset = copy.deepcopy(strToFloatDataset)
+standarized_dataset = copy.deepcopy(strToFloatDataset)
+train_test_split_dataset = copy.deepcopy(strToFloatDataset)
 normalize_dataset(normalized_dataset, minmax)
-standarized_dataset = dataset.copy()
 standardize_dataset(standarized_dataset, means, stdevs)
-train_test_split(dataset)
+train, test = train_test_split(train_test_split_dataset)
+print("\n- Primeras 10 filas dataset estandarizado:")
+for i in range(10):
+    print(standarized_dataset[i])
+print("\n- Primeras 10 filas dataset normalizado:")
+for i in range(10):
+    print(normalized_dataset[i])
+print("\n- Primeras 10 filas dataset de train:")
+for i in range(10):
+    print(train[i])
+print("\n- Primeras 10 filas dataset de train:")
+for i in range(10):    
+    print(test[i])
