27c1408d242d4fd45b5858efffbaa420f53439ee
Federico Becona <federicobecona@gmail.com>
2021-09-28 14:01:40 -0300

    Web update

--- ut2/PDs/UT2-PD4/scriptEj2.py
+++ ut2/PDs/UT2-PD4/scriptEj2.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
 
 input_file = "titanic.csv"
 dataset = pd.read_csv(input_file, header=0)
 
-#Probabilidad de que haya sobrevivido una persona con género G y clase de pasajero C
 def probSgivenGandC(G, C):
     size = len(dataset)
     sizeGC = len(dataset[(dataset['sex'] == G) & (dataset['pclass'] == C)])
@@ -13,12 +10,13 @@ def probSgivenGandC(G, C):
     probGC = sizeGC/size
     probSGC = sizeSGC/size
     return probSGC/probGC
-print("female, class 1, " + str(probSgivenGandC("female",1)))
-print("female, class 2, " + str(probSgivenGandC("female",2)))
-print("female, class 3, " + str(probSgivenGandC("female",3)))
-print("male, class 1, " + str(probSgivenGandC("male",1)))
-print("male, class 2, " + str(probSgivenGandC("male",2)))
-print("male, class 3, " + str(probSgivenGandC("male",3)))
+print("\n-Probabilidades:")
+print("*female, class 1, " + str(probSgivenGandC("female",1)))
+print("*female, class 2, " + str(probSgivenGandC("female",2)))
+print("*female, class 3, " + str(probSgivenGandC("female",3)))
+print("*male, class 1, " + str(probSgivenGandC("male",1)))
+print("*male, class 2, " + str(probSgivenGandC("male",2)))
+print("*male, class 3, " + str(probSgivenGandC("male",3)))
 
 def probSgivenMaxAandC(A, C):
     size = len(dataset)
@@ -27,4 +25,4 @@ def probSgivenMaxAandC(A, C):
     probMaxAC = sizeMaxAC/size
     probSMaxAC = sizeSMaxAC/size
     return probSMaxAC/probMaxAC
-print("Less than 3yo, class 3, " + str(probSgivenMaxAandC(10,3)))
+print("*Less than 3yo, class 3, " + str(probSgivenMaxAandC(10,3)))
