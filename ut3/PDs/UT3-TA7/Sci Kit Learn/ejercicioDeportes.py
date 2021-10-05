9f05bb757e1db88684d7d90677e05ee70f558e34
Federico Becona <federicobecona@gmail.com>
2021-10-05 17:54:07 -0300

    Update

---
+++ ut3/PDs/UT3-TA7/Sci Kit Learn/ejercicioDeportes.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+
+labels = ['Edad','Fuerza','Velocidad','Lesiones','Vision','Resistencia','Agilidad','CapacidadDecision']
+
+input_file = "sports_Training.csv"
+input_file2 = "sports_Scoring.csv"
+data_training = pd.read_csv(input_file, header=0)
+data_scoring = pd.read_csv(input_file2, header=0)
+
+data_training = data_training[(data_training['CapacidadDecision'] >= 3) & (data_training['CapacidadDecision'] <= 100)]
+data_scoring = data_scoring[(data_scoring['CapacidadDecision'] >= 3) & (data_scoring['CapacidadDecision'] <= 100)]
+
+scaler = StandardScaler()
+data_training[labels] = scaler.fit_transform(data_training[labels])
+data_scoring[labels] = scaler.fit_transform(data_scoring[labels])
+
+X = data_training[labels].values
+Y = data_training['DeportePrimario'].values
+
+lda = LinearDiscriminantAnalysis()
+lda = lda.fit(X, Y)
+
+Xsco = data_scoring[labels].values
+y_pred = lda.predict(Xsco)
+df = pd.DataFrame({'Prediccion': y_pred})
+writer = pd.ExcelWriter('Prediccion.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
+writer.save()
--- web/src/assets/downloads/Titanic Logistic Regression.py
+++ web/src/assets/downloads/Titanic Logistic Regression.py
@@ -1,48 +1,50 @@
 import pandas as pd 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import StratifiedKFold
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from numpy import mean
+from numpy import std
 
 input_file = "titanic.csv"
 dataset = pd.read_csv(input_file, header=0)
 
 dataset.drop(labels = ["body", "cabin","pclass","name","ticket"], axis = 1, inplace = True)
+
 dataset["age"].fillna(dataset["age"].mean(), inplace = True)
-dataset.dropna(subset=['fare'], how='all', inplace=True)
-dataset.dropna(subset=['embarked'], how='all', inplace=True)
+
 dataset["boat"].fillna('0', inplace = True)
 dataset["home.dest"].fillna('0', inplace = True)
 
-le_sex = LabelEncoder()
-le_sex.fit(dataset["sex"])
-encoded_sex_training = le_sex.transform(dataset["sex"])
+
+dataset = dataset[dataset['sibsp'] <= 5]
+dataset = dataset[dataset['parch'] <= 5]
+dataset = dataset[dataset['age'] <= 70]
+dataset = dataset[dataset['fare'] <= 350]
+dataset.dropna(subset=['fare'], how='all', inplace=True)
+dataset.dropna(subset=['embarked'], how='all', inplace=True)
+
+
+le = LabelEncoder()
+le.fit(dataset["sex"])
+encoded_sex_training = le.transform(dataset["sex"])
 dataset["sex"] = encoded_sex_training
-
-le_embarked = LabelEncoder()
-le_embarked.fit(dataset["embarked"])
-encoded_embarked_training = le_embarked.transform(dataset["embarked"])
+le.fit(dataset["embarked"])
+encoded_embarked_training = le.transform(dataset["embarked"])
 dataset["embarked"] = encoded_embarked_training
-
-le_embarked = LabelEncoder()
-le_embarked.fit(dataset["home.dest"])
-encoded_embarked_training = le_embarked.transform(dataset["home.dest"])
+le.fit(dataset["home.dest"])
+encoded_embarked_training = le.transform(dataset["home.dest"])
 dataset["home.dest"] = encoded_embarked_training
-
-le_embarked = LabelEncoder()
-le_embarked.fit(dataset["boat"])
-encoded_embarked_training = le_embarked.transform(dataset["boat"])
+le.fit(dataset["boat"])
+encoded_embarked_training = le.transform(dataset["boat"])
 dataset["boat"] = encoded_embarked_training
 
-print(dataset.keys)
 
 X = dataset.drop(labels=["survived"], axis=1)
 y = dataset["survived"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,random_state=0, shuffle=True)
-
-model = LogisticRegression(solver='liblinear', random_state=0)
-model.fit(X_train, y_train)
-pred_logreg = model.predict(X_test)
-acc_logreg = accuracy_score(y_test, pred_logreg)
-print("The Score for Logistic Regression is: " + str(acc_logreg))
+X, y = make_classification(n_samples=1283, n_features=10)
+cv = StratifiedKFold(n_splits=10, random_state=None, shuffle=True)
+model = LogisticRegression()
+scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
