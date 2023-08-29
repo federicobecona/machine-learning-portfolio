a84683f577e0d1d24c8524abc078081bb3584eec
fede <federicobecona@gmail.com>
2023-08-29 20:16:04 -0300

    fixing cases

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -28,8 +28,9 @@ export class HomeComponent implements OnInit {
   }
   
   goToBlog(paramName:string):void{
+    const sanitizedParamName = encodeURIComponent(paramName.toLowerCase().replace(/\s+/g, '-'));  
     let actualURL = this.router.parseUrl(this.router.url);
-    let newURL = this.router.createUrlTree(['blog', 'case', paramName]);
+    let newURL = this.router.createUrlTree(['blog', 'case', sanitizedParamName]);
     newURL.queryParams['lang'] = actualURL.queryParamMap.get('lang');
     this.router.navigateByUrl(newURL); 
   }
